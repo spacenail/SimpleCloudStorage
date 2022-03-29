2f4d353e9cd85cbe0b167a14c8134806beedbd17
Artem Meshcherov <spacenail@gmail.com>
2022-03-29 23:09:32 +0300

    first stage

--- src/main/java/com/github/spacenail/SimpleCloudStorage/client/ClientController.java
+++ src/main/java/com/github/spacenail/SimpleCloudStorage/client/ClientController.java
@@ -1,8 +1,12 @@
 package com.github.spacenail.SimpleCloudStorage.client;
 
 import com.github.spacenail.SimpleCloudStorage.model.*;
-import io.netty.handler.codec.serialization.ObjectDecoderInputStream;
-import io.netty.handler.codec.serialization.ObjectEncoderOutputStream;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.serialization.*;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,7 +16,6 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
-import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -29,12 +32,11 @@ public class ClientController implements Initializable {
     private TextField clientPath;
     @FXML
     private TextField serverPath;
-    private ObjectEncoderOutputStream objectEncoderOutputStream;
-    private ObjectDecoderInputStream objectDecoderInputStream;
     private Path clientDirectory;
+    private ChannelHandlerContext ctx;
 
-    public void download() throws IOException {
-        objectEncoderOutputStream.writeObject(new FileRequestMessage(
+    public void download() {
+        ctx.writeAndFlush(new FileRequestMessage(
                 getPath(serverPath.getText(), serverView.getSelectionModel().getSelectedItem())
                         .normalize().toAbsolutePath().toString(),
                 clientPath.getText())
@@ -42,7 +44,7 @@ public class ClientController implements Initializable {
     }
 
     public void upload() throws IOException {
-        objectEncoderOutputStream.writeObject(new FileMessage(
+        ctx.writeAndFlush(new FileMessage(
                 getPath(clientPath.getText(), clientView.getSelectionModel().getSelectedItem()),
                 serverPath.getText())
         );
@@ -75,30 +77,6 @@ public class ClientController implements Initializable {
         });
     }
 
-    private void read() {
-        while (true) {
-            try {
-                CloudMessage message = (CloudMessage) objectDecoderInputStream.readObject();
-                switch (message.getMessageType()) {
-                    case FILE:
-                        FileMessage fileMessage = (FileMessage) message;
-                        Files.write(Paths.get(fileMessage.getPath())
-                                        .resolve(fileMessage.getName())
-                                , fileMessage.getBytes()
-                        );
-                        updateView(clientDirectory);
-                        break;
-                    case LIST:
-                        ListMessage listMessage = (ListMessage) message;
-                        updateView(listMessage);
-                        break;
-                }
-            } catch (ClassNotFoundException | IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     private void clientViewHandler(MouseEvent event) {
         if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
             String item = clientView.getSelectionModel().getSelectedItem();
@@ -114,38 +92,64 @@ public class ClientController implements Initializable {
     }
 
     private void serverViewHandler(MouseEvent event) {
-        try {
-            if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
-                String item = serverView.getSelectionModel().getSelectedItem();
-                if ("...".equals(item)) {
-                    objectEncoderOutputStream.writeObject(new ListRequestMessage(serverPath.getText(), ".."));
-                } else if (item != null) {
-                    objectEncoderOutputStream.writeObject(new ListRequestMessage(serverPath.getText(), item));
-                }
+        if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
+            String item = serverView.getSelectionModel().getSelectedItem();
+            if ("...".equals(item)) {
+                ctx.writeAndFlush(new ListRequestMessage(serverPath.getText(), ".."));
+            } else if (item != null) {
+                ctx.writeAndFlush(new ListRequestMessage(serverPath.getText(), item));
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        EventLoopGroup executors = new NioEventLoopGroup(1);
         try {
-            Socket socket = new Socket("localhost", 8189);
-            objectEncoderOutputStream = new ObjectEncoderOutputStream(socket.getOutputStream());
-            objectDecoderInputStream = new ObjectDecoderInputStream(socket.getInputStream());
+            ChannelFuture channelFuture = networkInit(executors).connect("localhost", 8189).sync();
+            ctx = channelFuture.channel().pipeline().context("logic");
             clientDirectory = Paths.get("ClientDirectory").toAbsolutePath();
-
             clientView.setOnMouseClicked(this::clientViewHandler);
             serverView.setOnMouseClicked(this::serverViewHandler);
-
             updateView(clientDirectory);
-            Thread readThread = new Thread(this::read);
-            readThread.setDaemon(true);
-            readThread.start();
-        } catch (IOException e) {
+
+        } catch (InterruptedException e) {
             e.printStackTrace();
+        }finally {
+            executors.shutdownGracefully();
         }
+    }
+
+    private Bootstrap networkInit(EventLoopGroup executors) {
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.channel(NioSocketChannel.class);
+        bootstrap.group(executors);
+        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) {
+                socketChannel.pipeline().addFirst("decoder", new ObjectDecoder(ClassResolvers.cacheDisabled(null)));
+                socketChannel.pipeline().addLast("encoder", new ObjectEncoder());
+                socketChannel.pipeline().addLast("logic", new SimpleChannelInboundHandler<CloudMessage>() {
+                    @Override
+                    protected void channelRead0(ChannelHandlerContext ctx, CloudMessage message) throws Exception {
+                        switch (message.getMessageType()) {
+                            case FILE:
+                                FileMessage fileMessage = (FileMessage) message;
+                                Files.write(Paths.get(fileMessage.getPath())
+                                        .resolve(fileMessage.getName()), fileMessage.getBytes()
+                                );
+                                updateView(clientDirectory);
+                                break;
+                            case LIST:
+                                ListMessage listMessage = (ListMessage) message;
+                                updateView(listMessage);
+                                break;
+                        }
+                    }
+                });
+            }
+        });
+        return bootstrap;
     }
 }
